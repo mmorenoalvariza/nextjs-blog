7a8ef50354cde7597349add9afa43c7e7726d4a3
Mariano Moreno Alvariza <mmorenoalvariza@gmail.com>
2022-01-08 18:15:33 -0300

    Add movies

--- components/Movie.tsx
+++ components/Movie.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const MovieFC: FunctionComponent<{ movie: Movie }> = ({ movie }) => {
     return (<div key={movie._id} style={{ margin: '10px', border: '1px solid black', padding: '10px', borderRadius: '4px' }}>
         <div><label>Id:</label>
-            <Link key={movie._id} href={`/movies/${movie._id}`}>{movie._id}</Link></div>
+            <Link key={movie._id} href={`/ssp/${movie._id}`}>{movie._id}</Link></div>
         <div><label>Title:</label>{movie.title}</div>
         <div><label>Year:</label>{movie.year}</div>
         <div><label>Runtime:</label>{movie.runtime}</div>
