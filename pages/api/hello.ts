cdde678b3b3132f7c727f80edb4455cc6b0e3822
Mariano Moreno Alvariza <mmorenoalvariza@gmail.com>
2022-01-08 20:05:24 -0300

    Api with PUT

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,23 @@
+import { ObjectId } from "mongodb";
 import { connectToDatabase } from "../../lib/mongodb";
 import { COLLECTION } from "../../lib/movies";
 
 export default async function handler(req, res) {
-    const { method, body } = req;
+    const { method, body, query } = req;
+    const id = query.id as string;
     const db = await connectToDatabase();
-    if ('POST' === method) {
-
-        const newMovie = await db.collection(COLLECTION).insertOne(body);
-
-        res.status(201).json(newMovie);
-    } else {
-        res.status(200).json(await db.collection(COLLECTION).count({ year: 1989, runtime: 81 }))
+    const collection = db.collection(COLLECTION);
+    switch (method) {
+        case 'POST': {
+            const newMovie = await collection.insertOne(body);
+            res.status(201).json(newMovie);
+        }
+        case 'GET': {
+            res.status(200).json(await collection.findOne({ _id: new ObjectId(id) }))
+        }
+        case 'PUT': {
+            await collection.replaceOne({ _id: new ObjectId(id) }, body);
+            res.status(201).json(await collection.findOne({ _id: new ObjectId(id) }))
+        }
     }
-}
+};
